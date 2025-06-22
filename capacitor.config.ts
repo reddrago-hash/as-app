17f5fda5160cf54f8c123daa39ea836671602dc1
Andrew Singleton <asingleton4@uagrantham.edu>
2025-06-22 11:04:52 -1000

    week6-1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,20 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'as_app',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // in ms
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff", // white background
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      spinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false
+    }
+  }
 };
 
 export default config;
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 
+interface Scene {
+  text: string;
+  choices: { text: string; next: string }[];
+}
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: false,
 })
+
 export class Tab1Page {
+  scenes: { [key: string]: Scene } = {
+    start: {
+      text: "You are walking through the woods and see a bridge, it must be crossed. As you begin to cross the bridge, a troll jumps out!",
+      choices: [
+        { text: "Attack!", next: "mush" },
+        { text: "Hear him out", next: "riddle" }
+      ]
+    },
+    mush: {
+      text: "The troll turns you into mush. Game over",
+      choices: [{text: "Retry?", next: "start"}]
+    },
+    riddle: {
+      text: "The troll asks: What gets shorter as it gets older?",
+      choices: [
+        { text: "Chicken", next: "mush"},
+        { text: "Candle", next: "win"},
+        { text: "Hair", next: "mush"},
+        { text: "Time", next: "mush"}
+      ]
+    },
+    win: {
+      text: "You are allowed to pass, you win",
+      choices: []
+    }
+  };
 
-  constructor() {}
+  currentScene = this.scenes['start'];
 
+  makeChoice(nextSceneKey: string) {
+    this.currentScene = this.scenes[nextSceneKey];
+  }
 }
+
